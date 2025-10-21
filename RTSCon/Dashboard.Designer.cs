ecb938ff063ad650da4be96263cf81f2224c151d
Manuel Feliz <83149840+MaFeLop@users.noreply.github.com>
2025-10-21 14:58:35 -0400

    Primer Borrador Dashboard

---
+++ RTSCon/Dashboard.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace RTSCon
+{
+    partial class Dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.DashboardPanel = new Krypton.Toolkit.KryptonPanel();
+            this.kryptonLabel1 = new Krypton.Toolkit.KryptonLabel();
+            this.DashboardName = new Krypton.Toolkit.KryptonLabel();
+            this.BorderTopBar = new Krypton.Toolkit.KryptonBorderEdge();
+            this.BorderDashboard = new Krypton.Toolkit.KryptonBorderEdge();
+            this.kryptonManager1 = new Krypton.Toolkit.KryptonManager(this.components);
+            this.CondominiosBorde = new Krypton.Toolkit.KryptonBorderEdge();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.DashboardPanel)).BeginInit();
+            this.DashboardPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // DashboardPanel
+            // 
+            this.DashboardPanel.Controls.Add(this.label2);
+            this.DashboardPanel.Controls.Add(this.label1);
+            this.DashboardPanel.Controls.Add(this.CondominiosBorde);
+            this.DashboardPanel.Controls.Add(this.kryptonLabel1);
+            this.DashboardPanel.Controls.Add(this.DashboardName);
+            this.DashboardPanel.Controls.Add(this.BorderTopBar);
+            this.DashboardPanel.Controls.Add(this.BorderDashboard);
+            this.DashboardPanel.Location = new System.Drawing.Point(-5, 0);
+            this.DashboardPanel.Name = "DashboardPanel";
+            this.DashboardPanel.PaletteMode = Krypton.Toolkit.PaletteMode.Microsoft365SilverLightMode;
+            this.DashboardPanel.Size = new System.Drawing.Size(1201, 632);
+            this.DashboardPanel.TabIndex = 0;
+            // 
+            // kryptonLabel1
+            // 
+            this.kryptonLabel1.AutoSize = false;
+            this.kryptonLabel1.LabelStyle = Krypton.Toolkit.LabelStyle.BoldControl;
+            this.kryptonLabel1.Location = new System.Drawing.Point(142, 103);
+            this.kryptonLabel1.Name = "kryptonLabel1";
+            this.kryptonLabel1.PaletteMode = Krypton.Toolkit.PaletteMode.Microsoft365SilverLightMode;
+            this.kryptonLabel1.Size = new System.Drawing.Size(252, 44);
+            this.kryptonLabel1.StateCommon.LongText.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kryptonLabel1.StateCommon.ShortText.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kryptonLabel1.TabIndex = 4;
+            this.kryptonLabel1.Values.Text = "Vista previa del periodo: ";
+            this.kryptonLabel1.Click += new System.EventHandler(this.kryptonLabel1_Click);
+            // 
+            // DashboardName
+            // 
+            this.DashboardName.AutoSize = false;
+            this.DashboardName.LabelStyle = Krypton.Toolkit.LabelStyle.BoldControl;
+            this.DashboardName.Location = new System.Drawing.Point(142, 49);
+            this.DashboardName.Name = "DashboardName";
+            this.DashboardName.PaletteMode = Krypton.Toolkit.PaletteMode.Microsoft365SilverLightMode;
+            this.DashboardName.Size = new System.Drawing.Size(198, 57);
+            this.DashboardName.StateCommon.LongText.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DashboardName.StateCommon.ShortText.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DashboardName.TabIndex = 3;
+            this.DashboardName.Values.Text = "Dashboard";
+            // 
+            // BorderTopBar
+            // 
+            this.BorderTopBar.AutoSize = false;
+            this.BorderTopBar.BorderStyle = Krypton.Toolkit.PaletteBorderStyle.ControlAlternate;
+            this.BorderTopBar.Location = new System.Drawing.Point(142, 0);
+            this.BorderTopBar.Name = "BorderTopBar";
+            this.BorderTopBar.PaletteMode = Krypton.Toolkit.PaletteMode.Microsoft365BlackDarkModeAlternate;
+            this.BorderTopBar.Size = new System.Drawing.Size(1059, 43);
+            this.BorderTopBar.Text = "kryptonBorderEdge1";
+            // 
+            // BorderDashboard
+            // 
+            this.BorderDashboard.AutoSize = false;
+            this.BorderDashboard.Location = new System.Drawing.Point(0, 0);
+            this.BorderDashboard.Name = "BorderDashboard";
+            this.BorderDashboard.PaletteMode = Krypton.Toolkit.PaletteMode.Microsoft365BlackDarkMode;
+            this.BorderDashboard.Size = new System.Drawing.Size(142, 629);
+            this.BorderDashboard.Text = "kryptonBorderEdge1";
+            // 
+            // CondominiosBorde
+            // 
+            this.CondominiosBorde.AutoSize = false;
+            this.CondominiosBorde.Location = new System.Drawing.Point(204, 153);
+            this.CondominiosBorde.Name = "CondominiosBorde";
+            this.CondominiosBorde.PaletteMode = Krypton.Toolkit.PaletteMode.Microsoft365BlackDarkMode;
+            this.CondominiosBorde.Size = new System.Drawing.Size(163, 126);
+            this.CondominiosBorde.Text = "kryptonBorderEdge1";
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(212, 162);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 23);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Condominios Activos:";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(293, 207);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "label2";
+            // 
+            // Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1193, 625);
+            this.Controls.Add(this.DashboardPanel);
+            this.Location = new System.Drawing.Point(0, 0);
+            this.Name = "Dashboard";
+            this.Text = "RTSCon: Sistema de Gestión de Condominios";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DashboardPanel)).EndInit();
+            this.DashboardPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Krypton.Toolkit.KryptonPanel DashboardPanel;
+        private Krypton.Toolkit.KryptonBorderEdge BorderTopBar;
+        private Krypton.Toolkit.KryptonBorderEdge BorderDashboard;
+        private Krypton.Toolkit.KryptonManager kryptonManager1;
+        private Krypton.Toolkit.KryptonLabel DashboardName;
+        private Krypton.Toolkit.KryptonLabel kryptonLabel1;
+        private Krypton.Toolkit.KryptonBorderEdge CondominiosBorde;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ RTSCon/Dashboard.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Krypton.Toolkit;
+
+namespace RTSCon
+{
+    public partial class Dashboard : KryptonForm
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            var km = new KryptonManager();
+            km.GlobalPaletteMode = PaletteMode.Office2010BlueLightMode; // o el que prefieras
+        }
+
+        private void kryptonLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void kryptonLabel2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void kryptonLabel2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
