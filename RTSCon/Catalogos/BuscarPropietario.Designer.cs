f48078a63f1f84c408a6367533ea0611ba46ff35
Manuel Feliz <felizmanuel512@gmail.com>
2026-03-12 11:45:37 -0400

    Update BuscarPropietario.Designer and VS layout
    Modify the BuscarPropietario.Designer.cs (UI/form designer changes) and update Visual Studio workspace files (.suo and DocumentLayout.backup.json/DocumentLayout.json) to reflect updated document monikers, indices and window/layout state. The .suo is a binary workspace state file; DocumentLayout entries were adjusted to include the designer file and reorder document indices.

--- RTSCon/Catalogos/BuscarPropietario.Designer.cs
+++ RTSCon/Catalogos/BuscarPropietario.Designer.cs
@@ -46,7 +46,7 @@
             this.label2.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label2.Location = new System.Drawing.Point(13, -6);
+            this.label2.Location = new System.Drawing.Point(13, 9);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(327, 28);
@@ -144,7 +144,6 @@
             this.Controls.Add(this.btnLimpiarFiltros);
             this.Controls.Add(this.dgvPropietario);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Location = new System.Drawing.Point(0, 0);
             this.Name = "BuscarPropietario";
             this.Text = "Buscar Propietario";
             ((System.ComponentModel.ISupportInitialize)(this.dgvPropietario)).EndInit();
