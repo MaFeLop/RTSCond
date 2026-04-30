ccd21c7df61cdf66a7b99965f8c107a0ff80e529
Manuel Feliz <felizmanuel512@gmail.com>
2026-04-30 12:58:23 -0400

    Recreado FrmSesionExpirada por error garrafal

--- RTSCon/FrmSesionExpirada.Designer.cs
+++ RTSCon/FrmSesionExpirada.Designer.cs
@@ -28,19 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmSesionExpirada));
-            this.SuspendLayout();
-            // 
-            // FrmSesionExpirada
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(833, 473);
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Name = "FrmSesionExpirada";
-            this.Text = "Sesión Expirada";
-            this.ResumeLayout(false);
-
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "FrmSesionExpirada";
         }
 
         #endregion
