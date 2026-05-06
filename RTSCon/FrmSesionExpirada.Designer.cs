3ad4d25ba51722b15a53b60c6279d342664ac2a6
Manuel Feliz <felizmanuel512@gmail.com>
2026-05-06 09:25:49 -0400

    Sesion Expirada cambiada por 10x vez

--- RTSCon/FrmSesionExpirada.Designer.cs
+++ RTSCon/FrmSesionExpirada.Designer.cs
@@ -28,12 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmSesionExpirada));
+            this.kryptonLabel1 = new Krypton.Toolkit.KryptonLabel();
+            this.btnVolver = new Krypton.Toolkit.KryptonButton();
+            this.SuspendLayout();
+            // 
+            // kryptonLabel1
+            // 
+            this.kryptonLabel1.AutoSize = false;
+            this.kryptonLabel1.LabelStyle = Krypton.Toolkit.LabelStyle.BoldPanel;
+            this.kryptonLabel1.Location = new System.Drawing.Point(12, 25);
+            this.kryptonLabel1.Name = "kryptonLabel1";
+            this.kryptonLabel1.Size = new System.Drawing.Size(714, 48);
+            this.kryptonLabel1.StateCommon.ShortText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kryptonLabel1.TabIndex = 0;
+            this.kryptonLabel1.Values.Text = "Su sesión fue expirada por inactividad, por favor vuelva a iniciar sesión";
+            // 
+            // btnVolver
+            // 
+            this.btnVolver.Location = new System.Drawing.Point(276, 136);
+            this.btnVolver.Margin = new System.Windows.Forms.Padding(4);
+            this.btnVolver.Name = "btnVolver";
+            this.btnVolver.PaletteMode = Krypton.Toolkit.PaletteMode.Microsoft365BlueDarkMode;
+            this.btnVolver.Size = new System.Drawing.Size(179, 55);
+            this.btnVolver.StateCommon.Content.ShortText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVolver.TabIndex = 22;
+            this.btnVolver.Values.DropDownArrowColor = System.Drawing.Color.Empty;
+            this.btnVolver.Values.Text = "Volver al Inicio";
+            // 
+            // FrmSesionExpirada
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FrmSesionExpirada";
+            this.ClientSize = new System.Drawing.Size(738, 302);
+            this.Controls.Add(this.btnVolver);
+            this.Controls.Add(this.kryptonLabel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmSesionExpirada";
+            this.Text = "Sesión Expirada";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Krypton.Toolkit.KryptonLabel kryptonLabel1;
+        private Krypton.Toolkit.KryptonButton btnVolver;
     }
 }
